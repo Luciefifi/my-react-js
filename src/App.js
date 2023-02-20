b6550b9419fecc4e6f68ebc1d7e03fd3af1ac041
Luciefifi <niyomutonilucie@gmail.com>
2023-02-20 09:07:22 +0300

    to do list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
 
-import './App.css';
-import { useState } from 'react';
-function App() {
-const [count , setCount] = useState(0)
+                import './App.css';
+                import { useState } from 'react';
+                function App() {
+                var [todoList , setTodoList] = useState([])
+                const [newTask , setNewTask] = useState("")
 
-const increase = () =>{
-setCount(count+1)
-}
+                const handleChange = (event) =>{
+                  setNewTask(event.target.value)
+                  
+                }
 
-const decrease = ( ) =>{
-  setCount(count-1)
-}
+                const addTask = () =>{
+                const task = {
+                    id:todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+                    taskName : newTask
+                  }
+                  setTodoList( [...todoList , task])
+                }
 
-const setToZero = () =>{
-  setCount(0)
-}
+                const deleteTask =  (id) => {
+                const newTodoList = todoList.filter((task) =>{
+                  if(task.id === id)
+                  {
+                    return false
+                  }
+                  else{
+                    return true
+                  }
 
-return(
- <div  className="App">
- <button  style = {{ backgroundColor : "green"}} onClick={increase}>Increase</button> 
- <button  style = {{ backgroundColor : "red"}} onClick={decrease}>decrease</button>
- <button  style = {{ backgroundColor : "yellow"}} onClick={setToZero}>set to zero</button>
-{count}
- </div>
-)
-}
-
-
+                })
+                setTodoList(newTodoList)
+                }
 
 
-export default App;
+
+                return(
+                <div  className="App">
+                <div className='task'>
+                <input onChange={handleChange}/>
+                <button onClick={addTask}>Add Task</button>
+                </div>
+                <div className='List'>
+                  {
+                    todoList.map((task) =>{
+                      
+                return <div><h1>{task.taskName}</h1>
+                <button onClick={()=> deleteTask(task.id)}>X</button></div>
+
+                    })
+                  }
+                </div>
+
+
+
+
+
+                </div>
+                )
+                }
+
+
+
+
+                export default App;
